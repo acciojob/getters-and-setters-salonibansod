72860b0d1edebef010ce5a377bed8b75bd915278
salonibansod <147966433+salonibansod@users.noreply.github.com>
2025-01-23 18:05:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 //complete this code
-class Person {}
+class Person {
+    constructor(name) {
+        this.name = name;
+    }
+}
 
-class Student extends Person {}
+class Student extends Person {
+    study() {
+        console.log(`${this.name} is studying`);
+    }
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+    teach() {
+        console.log(`${this.name} is teaching`);
+    }
+}
 
 // Do not change the code below this line
 window.Person = Person;
